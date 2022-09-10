6e70ab64ec821a80a4e71a909f2fea308eb429db
imogawa <ivinymatsue@hotmail.com>
2022-09-10 16:48:36 -0300

    :tada: init

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Header from './components/Header/Header.jsx';
+import Form from './components/Formulario/Form.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [pagina, setPagina] = useState(1);
+
+  const [dado, setDado] = useState([]);
+
+
+  const apiUrl = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1`
+
+  const getDado = async () => {
+
+    try {
+      let res = await fetch(apiUrl)
+      let dadoTransformado = await res.json()
+
+      setDado([...dado, ...dadoTransformado.products])
+    } catch (error) {
+      alert("Erro!");
+    }
+  }
+
+  const informacoes = dado.map(
+    (element) =>
+      <>
+        <div className='conteudo'>
+          <h3> {element.name} </h3>
+          <figure>
+            <img className='fotoProduto' src={element.image} alt='Foto do produto' />
+          </figure>
+          <h5> {element.description} </h5>
+          <h4><s> {'De R$:' + element.oldPrice} </s></h4>
+          <h3><em> {'Por R$:' + element.price} </em></h3>
+          <button className='botao'>Comprar</button>
+        </div>
+      </>
+  );
+
+  const mostrarMais = () => {
+    setPagina(pagina + 1)
+  };
+
+  useEffect(() => {
+    getDado()
+  }, [pagina])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Form />
+      <section className="center">{informacoes} 
+        <button className="botaoMostra" onClick={mostrarMais}>Ainda mais produtos aqui</button>
+        </section>
+        <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
